f2974b2955e11c483b192b27957a7240592f3557
Jon Toshmatov from HP <jontoshmatov@yahoo.com>
2020-07-12 18:22:51 -0400

    saving the last class

--- js/calculator.js
+++ js/calculator.js
@@ -1,22 +1,59 @@
+let number = '';
+let number1 = '';
+let number2 = '';
+let sign = '';
+let formula = '';
 $(function () {
-    let number = null;
-    let number1 = null;
-    let number2 = null;
-    let sign = null;
+    //Raqamlarni bosganizda bajaralyotgan harakat
     $(".num").click(function () {
         number = $(this).text();
-        number += number;
-        if (sign===''){
-            number1 = number;
+        if (sign !== '') {
+            number1 += number;
             number = '';
+        } else {
+            number2 += number;
         }
-        display_result(number1 +sign+ number2);
+        display_result(number2 + sign + number1);
+        formula = eval(number2 + sign + number1);
     });
+    //Belgilarni bosganizda bajaraliyotgan harakat
     $(".sign").click(function () {
+        //Bosilgan beligini toplash
         sign = $(this).text();
-        display_result(sign);
+        //Belgilarni HTML entity formatida oddiy mathematika operator
+        //beligisiga konvertatsiya qilish
+        if (sign === '÷') {
+            sign = '/';
+        }
+        if (sign === '×') {
+            sign = '*';
+        }
+        if (sign === '−') {
+            sign = '-';
+        }
+        if (sign === 'AC') {
+            Ac_button();
+        }
+        if (sign === '=') {
+            display_result(formula);
+            Ac_button(false);
+        }
     });
 });
-function display_result(num){
+//Mathetika operatsiyasini natijasini ekranga chiqarish funktsiyasi
+function display_result(num) {
+    //result degan class div ichiga HTML formatida print qilish
     $(".result").html(num);
+}
+//Kalkyulatrni qayta o'rnatish funktsiyasi
+function Ac_button(reset = true) {
+    number = '';
+    number1 = '';
+    number2 = '';
+    sign = '';
+    //Agar reset degan o'zgaruvchan (Variable) to'gri bolsa
+    //Ekrandagi natijani 0 ga tenglashtirish
+    if (reset) {
+        display_result(0);
+    }
 }
